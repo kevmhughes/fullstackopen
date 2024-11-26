e896b7d41eba519b93c13f39ea8e55b98392db28
kevmhughes <kevhughes24@hotmail.com>
2024-11-26 12:12:04 +0100

    2.5: Separate module step 10

--- part2/course_information/src/App.jsx
+++ part2/course_information/src/App.jsx
@@ -1,37 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>;
-
-const Total = ({ total }) => <b>total of exercises {total}</b>;
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <>
-    {parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </>
-);
-
-const Course = ({ courses, total, title }) => {
-  return (
-    <>
-      <h1>{title}</h1>
-      <div>
-        {courses.map((course, i) => (
-          <div key={course.id}>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total total={total[i]} />
-          </div>
-        ))}
-      </div>
-    </>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
