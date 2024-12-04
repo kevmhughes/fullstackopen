fbfd6041a715073c7e93a4fbecc60484af14202b
kevmhughes <kevhughes24@hotmail.com>
2024-12-04 18:27:27 +0100

    4.4: Helper Functions and Unit Tests, step 2

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -1,7 +1,18 @@
 const dummy = (blogs) => {
-  return 1
-}
+  return 1;
+};
 
 module.exports = {
-  dummy
-}
+  dummy,
+};
+
+const totalLikes = (blogs) => {
+  const totalLikes = blogs.reduce((acc, blog) => acc + blog.likes, 0);
+
+  return totalLikes;
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+};
