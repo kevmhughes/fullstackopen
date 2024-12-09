c6dcb39055523b6fe1b2127c006b7518fdae07e0
kevmhughes <kevhughes24@hotmail.com>
2024-12-09 04:43:07 +0100

    4.12*: Blog List tests, step 5

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -5,7 +5,7 @@ const logger = require("../utils/logger");
 blogsRouter.get("/", async (request, response) => {
   try {
     const blogs = await Blog.find({});
-    response.status(200).json(blogs); 
+    response.status(200).json(blogs);
   } catch (err) {
     logger.error("Error fetching blogs:", err.message);
     response.status(500).json({ error: "Failed to fetch blogs" });
@@ -13,7 +13,26 @@ blogsRouter.get("/", async (request, response) => {
 });
 
 blogsRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
+  const { title, url, author, likes } = request.body;
+
+  // Check for missing required fields and provide specific error messages
+  if (!title) {
+    return response.status(400).json({ error: "Title is required" });
+  }
+  if (!url) {
+    return response.status(400).json({ error: "URL is required" });
+  }
+  if (!author) {
+    return response.status(400).json({ error: "Author is required" });
+  }
+
+  const blog = new Blog({
+    title,
+    url,
+    author,
+    likes: likes || 0, // Default likes to 0 if not provided
+  });
+
   try {
     const postedBlog = await blog.save();
     response.status(201).json(postedBlog);
--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require("node:test");
+const { test, after, describe, beforeEach } = require("node:test");
 const Blog = require("../models/blog.js");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
@@ -22,74 +22,104 @@ beforeEach(async () => {
   await blogObject.save();
 });
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
-
-test("all blogs are returned", async () => {
-  const response = await api.get("/api/blogs");
-
-  assert.strictEqual(response.body.length, helper.initialBlogs.length);
-});
-
-test("blogs have an id property instead of _id", async () => {
-  const response = await api.get("/api/blogs");
-
-  response.body.forEach((blog) => {
-    // Ensure that the blog has an 'id' property
-    assert(blog.hasOwnProperty("id"), `Blog does not have an 'id' property`);
-    // Ensure that the blog does not have an '_id' property
-    assert(
-      !blog.hasOwnProperty("_id"),
-      `Blog has an '_id' property, but it should not`
-    );
+describe("back end tests", () => {
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
   });
-});
 
-test("a valid blog can be added", async () => {
-  const newBlog = {
-    title: "A Day Out In The Park With Friends Four",
-    author: "Fanny Swipes & More Friends",
-    url: "https://disneyparksblog.com/community-outreach/disney-parks-costumers-sew-holiday-happiness-behind-the-scenes/",
-    likes: 1234,
-  };
+  test("all blogs are returned", async () => {
+    const response = await api.get("/api/blogs");
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    assert.strictEqual(response.body.length, helper.initialBlogs.length);
+  });
 
-  const blogsAtEnd = await helper.blogsInDb();
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+  test("blogs have an id property instead of _id", async () => {
+    const response = await api.get("/api/blogs");
 
-  const titles = blogsAtEnd.map((b) => b.title);
-  assert(titles.includes("A Day Out In The Park With Friends Four"));
-});
+    response.body.forEach((blog) => {
+      // Ensure that the blog has an 'id' property
+      assert(blog.hasOwnProperty("id"), `Blog does not have an 'id' property`);
+      // Ensure that the blog does not have an '_id' property
+      assert(
+        !blog.hasOwnProperty("_id"),
+        `Blog has an '_id' property, but it should not`
+      );
+    });
+  });
 
-test("if the likes property is missing it defaults to zero", async () => {
-  const blogWithoutLikes = {
-    title: "Starting Out",
-    author: "Fanny Smith",
-    url: "https://disneyparksblog.com/community-outreach/disney-parks-costumers-sew-holiday-happiness-behind-the-scenes/",
-  };
+  test("a valid blog can be added", async () => {
+    const newBlog = {
+      title: "A Day Out In The Park With Friends Four",
+      author: "Fanny Swipes & More Friends",
+      url: "https://disneyparksblog.com/community-outreach/disney-parks-costumers-sew-holiday-happiness-behind-the-scenes/",
+      likes: 1234,
+    };
 
-  await api
-    .post("/api/blogs")
-    .send(blogWithoutLikes)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-  const blogsAtEnd = await helper.blogsInDb();
-  // Ensure the number of blogs has increased by 1
-  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
-  // Check that the new blog has 'likes' set to 0
-  assert.strictEqual(blogsAtEnd[helper.initialBlogs.length].likes, 0);
-});
+    const blogsAtEnd = await helper.blogsInDb();
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
 
-after(async () => {
-  await mongoose.connection.close();
+    const titles = blogsAtEnd.map((b) => b.title);
+    assert(titles.includes("A Day Out In The Park With Friends Four"));
+  });
+
+  test("if the likes property is missing it defaults to zero", async () => {
+    const blogWithoutLikes = {
+      title: "Starting Out",
+      author: "Fanny Smith",
+      url: "https://disneyparksblog.com/community-outreach/disney-parks-costumers-sew-holiday-happiness-behind-the-scenes/",
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(blogWithoutLikes)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    // Ensure the number of blogs has increased by 1
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
+    // Check that the new blog has 'likes' set to 0
+    assert.strictEqual(blogsAtEnd[helper.initialBlogs.length].likes, 0);
+  });
+
+  test("if no url property is provided in the request, a 400 status response is given", async () => {
+    const blogWithoutUrl = {
+      title: "Starting Out",
+      author: "Fanny Smith",
+      likes: 2,
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(blogWithoutUrl)
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("if no title property is provided in the request, a 400 status response is given", async () => {
+    const blogWithoutTitle = {
+      author: "Fanny Smith",
+      url: "https://disneyparksblog.com/community-outreach/disney-parks-costumers-sew-holiday-happiness-behind-the-scenes/",
+      likes: 2,
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(blogWithoutTitle)
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  after(async () => {
+    await mongoose.connection.close();
+  });
 });
