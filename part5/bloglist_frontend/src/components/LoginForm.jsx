5c0864a6ac0040bd0a0e8e55cd5e8185b12d55d8
kevmhughes <kevhughes24@hotmail.com>
2025-01-15 01:26:20 +0100

    5.18: Blog List End To End Testing, step 2

--- part5/bloglist_frontend/src/components/LoginForm.jsx
+++ part5/bloglist_frontend/src/components/LoginForm.jsx
@@ -16,6 +16,7 @@ const LoginForm = ({
         <div>
           username
           <input
+            data-testid='username'
             type="text"
             value={username}
             name="Username"
@@ -25,6 +26,7 @@ const LoginForm = ({
         <div>
           password
           <input
+            data-testid='password'
             type="password"
             value={password}
             name="Password"
--- part5/playwright/tests/bloglists.test.js
+++ part5/playwright/tests/bloglists.test.js
@@ -1,12 +1,37 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
 
 describe("Blog app", () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post("http://localhost:3003/api/testing/reset");
+    await request.post("http://localhost:3003/api/users", {
+      data: {
+        name: "Matti Luukkainen",
+        username: "mluukkai",
+        password: "salainen",
+      },
+    });
+
     await page.goto("http://localhost:5173");
   });
 
   test("Login form is shown", async ({ page }) => {
-    const form = await page.locator("#login-form");
+    const form = page.locator("#login-form");
     await expect(form).toBeVisible();
+  });
+
+  test("succeeds with correct credentials", async ({ page }) => {
+    await page.getByTestId("username").fill("mluukkai");
+    await page.getByTestId("password").fill("salainen");
+    await page.getByRole("button", { name: "login" }).click();
+    await expect(page.getByText("Matti Luukkainen is logged-in")).toBeVisible();
+  });
+
+  test("fails with wrong credentials", async ({ page }) => {
+    await page.getByTestId("username").fill("John");
+    await page.getByTestId("password").fill("qwerty123!");
+    await page.getByRole("button", { name: "login" }).click();
+    const errorDiv = page.locator(".error");
+    await expect(errorDiv).toContainText("Invalid username or password.");
+    await expect(errorDiv).toHaveCSS("color", "rgb(255, 0, 0)");
   });
 });
