41a44c4bd7f802481129a3d1e2a314bd1367d3be
kevmhughes <kevhughes24@hotmail.com>
2025-01-31 00:59:37 +0100

    Exercise 6.21

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,50 +1,62 @@
-import { useQuery } from '@tanstack/react-query'
-import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
-import { getAnecdotes } from './requests'
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
+import { getAnecdotes, createAnecdote } from "./requests";
 
 const App = () => {
+  const queryClient = useQueryClient();
+
+  const newNoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["anecdotes"] });
+    },
+  });
+
+  const addAnecdote = async (content) => {
+    newNoteMutation.mutate({ content, votes: 0 });
+  };
 
   const result = useQuery({
-    queryKey: ['anecdotes'],
-    queryFn: getAnecdotes
-  })
-  console.log(JSON.parse(JSON.stringify(result)))
+    queryKey: ["anecdotes"],
+    queryFn: getAnecdotes,
+  });
+  console.log(JSON.parse(JSON.stringify(result)));
 
-  if ( result.isLoading ) {
-    return <div>loading data...</div>
+  if (result.isLoading) {
+    return <div>loading data...</div>;
   }
 
-  if ( result.isError ) {
-    return <div>Anecdote service not available due to problems with the server.</div>
+  if (result.isError) {
+    return (
+      <div>Anecdote service not available due to problems with the server.</div>
+    );
   }
 
-  const anecdotes = result.data
+  const anecdotes = result.data;
 
   const handleVote = (anecdote) => {
-    console.log('vote')
-  }
+    console.log("vote");
+  };
 
   return (
-    <div className='container'>
-      <h3 style={{marginTop: "1rem"}}>Anecdote app</h3>
-    
+    <div className="container">
+      <h3 style={{ marginTop: "1rem" }}>Anecdote app</h3>
+
       <Notification />
-      <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
+      <AnecdoteForm onSubmit={addAnecdote} />
+
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,21 +1,23 @@
-const AnecdoteForm = () => {
+const AnecdoteForm = ({ onSubmit }) => {
+  const onCreate = (event) => {
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+    if (content.length < 5) {
+      return alert("Anecdote must be at least 5 characters long.");
+    }
+    onSubmit(content);
+  };
 
-    const onCreate = (event) => {
-      event.preventDefault()
-      const content = event.target.anecdote.value
-      event.target.anecdote.value = ''
-      console.log('new anecdote')
-  }
-  
-    return (
-      <div>
-        <h3>create new</h3>
-        <form onSubmit={onCreate}>
-          <input name='anecdote' />
-          <button type="submit">create</button>
-        </form>
-      </div>
-    )
-  }
-  
-  export default AnecdoteForm
+  return (
+    <div>
+      <h3>create new</h3>
+      <form onSubmit={onCreate}>
+        <input name="anecdote" />
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+};
+
+export default AnecdoteForm;
